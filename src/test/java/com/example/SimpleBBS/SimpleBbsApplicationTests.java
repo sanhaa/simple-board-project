08941abde6adfc557590185d61d3b21770aac526
Sanhaa <livia3609@gmail.com>
2024-01-22 23:06:09 +0900

    init

---
+++ src/test/java/com/example/SimpleBBS/SimpleBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleBBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
